d6d839c126bb120268ed6b55d11670051e2ed394
EC2 Default User <ec2-user@ip-172-31-44-224.ec2.internal>
2020-10-24 10:04:22 +0000

    first admin-tyumonrireki

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,10 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order_products = Order_products.all
   end
 
   def update
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,17 @@
 class Order < ApplicationRecord
   has_many :ordered_products
   belongs_to :customer
+  
+  def order_total_amount
+    order_products = OrderdProduct.where(order_id: self.id)
+    quantity_list = []
+    order_products.each do |product|
+    quantity_list.push(product.ordered_quantity)
+  end
+    return quantity_list.sum
+  end
+  
+  def ordered_products_total_amoun
+  end
+  
 end
