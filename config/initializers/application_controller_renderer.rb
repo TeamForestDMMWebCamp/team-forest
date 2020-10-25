0e29e8bd61e74116e202eabfe01d7a2c6f3145c8
EC2 Default User <ec2-user@ip-172-31-67-89.ec2.internal>
2020-10-25 11:52:10 +0000

    [finish]アドミンの新規登録の削除、ログインしていない場合admin側入れなくした

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'eaf7d0d3c082164a120cb0c225afd160d982bbce1060178074726e0b9f76ec7acb93a7854ee038e66c014c49f89d53c15f834ff77b0bc737944978d2f5c16028'
