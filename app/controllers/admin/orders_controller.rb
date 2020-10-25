94d32ae87572f23123b64eda2b652108f4a75b39
EC2 Default User <ec2-user@ip-172-31-67-89.ec2.internal>
2020-10-25 06:18:29 +0000

    [fin]admin-top

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,23 +3,8 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 
   def update
-    @orderd_product = OrderProduct.find(params[:production_status])
-    @order = Order.find(params[:id])
-   if @order.update(payed)
-      @ordered_product.update
-     redirect_back(fallback_location: admin_order_path)
-     flash[notice] = '更新に成功しました'
-   else
-     render :show
-   end
   end
-  
-  private 
- def order_params
-   params.require(:order).permit(:order_received_status)
- end
 end
