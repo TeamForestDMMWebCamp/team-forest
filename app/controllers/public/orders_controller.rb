5d90b5ee7d9dc42084a7079087dcc34c8da5e0e8
EC2 Default User <ec2-user@ip-172-31-70-76.ec2.internal>
2020-10-26 09:39:22 +0000

    [Add] 新しい届け先で注文した場合に、配送先に新規登録

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,95 +1,106 @@
 class Public::OrdersController < ApplicationController
-  before_action :authenticate_customer!
+	before_action :authenticate_customer!
 
 	def index
-   	 @orders = current_customer.orders
+		 @orders = current_customer.orders
 	end
 
-  def show
-    @order = Order.find(params[:id])
-    @ordered_products = OrderedProduct.where(order_id: params[:id])
-  end
+	def show
+		@order = Order.find(params[:id])
+		@ordered_products = OrderedProduct.where(order_id: params[:id])
+	end
 
-  def information
-    
-    @shipping_addresses = current_customer.shipping_addresses
-  end
+	def information
 
-  def confirm
-    # ユーザーのカート内商品の取得
-    @cart_products = current_customer.cart_products
-    # ↓送料の指定
-    @shipping_fee = 800
+		@shipping_addresses = current_customer.shipping_addresses
+	end
 
-    # ↓お届け先の条件分岐
-    if params["radio"] == "r1"
-      # payment_methodのみ取得
-      @order_info = Order.new(order_select_params)
-      # 住所は会員情報から取得
-      render :new
+	def confirm
+		# ユーザーのカート内商品の取得
+		@cart_products = current_customer.cart_products
+		# ↓送料の指定
+		@shipping_fee = 800
 
-    elsif params["radio"] == "r2"
-      # payment_methodのみ取得
-      @order_info = Order.new(order_select_params)
-      # 住所は配送先一覧から取得
-      @shipping_address = ShippingAddress.find(params[:shipping_select])
-      render :new
+		# ↓お届け先の条件分岐
+		if params["radio"] == "r1"
+			# payment_methodのみ取得
+			@order_info = Order.new(order_select_params)
+			# 住所は会員情報から取得
+			render :new
 
-    elsif params["radio"] == "r3"
-      # text_fieldからもデータ取得
-      @order_info = Order.new(order_form_params)
-      render :new
-    else
-      #ラジオボタン自動選択なので、基本的には発生しない
-      flash[:notice] = "配送先が選択されていません"
-      render :information
-    end
-  end
+		elsif params["radio"] == "r2"
+			# payment_methodのみ取得
+			@order_info = Order.new(order_select_params)
+			# 住所は配送先一覧から取得
+			@shipping_address = ShippingAddress.find(params[:shipping_select])
+			render :new
 
-  def new
+		elsif params["radio"] == "r3"
+			# text_fieldからもデータ取得
+			@order_info = Order.new(order_form_params)
+			render :new
+		else
+			#ラジオボタン自動選択なので、基本的には発生しない
+			flash[:notice] = "配送先が選択されていません"
+			render :information
+		end
+	end
 
-  end
+	def new
 
-  def create
-    @order = Order.new(order_params)
-    @cart_products = current_customer.cart_products
+	end
 
-    if @order.save
-      @cart_products.each do |cart|
-        @ordered_product =  OrderedProduct.new
-        @ordered_product.order_id = @order.id
-        @ordered_product.product_id = cart.product.id
+	def create
+		@order = Order.new(order_params)
+		@cart_products = current_customer.cart_products
+
+		if @order.save
+			@cart_products.each do |cart|
+				@ordered_product =  OrderedProduct.new
+				@ordered_product.order_id = @order.id
+				@ordered_product.product_id = cart.product.id
 				@ordered_product.ordered_quantity = cart.quantity
 				@ordered_product.price_including_tax = cart.product.including_tax
 
 				@ordered_product.save
 			end
 
+			# お届け先が新規登録なら配送先一覧に追加
+			if params[:postal_code].present?
+				@shipping_address = ShippingAddress.new(shipping_params)
+				@shipping_address.customer_id = current_customer.id
+				@shipping_address.save
+			end
+
 			@cart_products.destroy_all
-      redirect_to orders_thanks_path
-    else
-      flash[:notice] = "情報の入力を完了してください"
-      redirect_to orders_information_path
-    end
-  end
+			redirect_to orders_thanks_path
+		else
+			flash[:notice] = "情報の入力を完了してください"
+			redirect_to orders_information_path
+		end
+	end
 
-  def thanks
-  end
+	def thanks
+	end
 
-  private
+	private
 
-  # 宛先選択のラジオボタンが１(現在住所)か２(配送先一覧から選択)だった場合
-  def order_select_params
-    params.permit(:payment_method)
-  end
+	# 宛先選択のラジオボタンが１(現在住所)か２(配送先一覧から選択)だった場合
+	def order_select_params
+		params.permit(:payment_method)
+	end
 
-  # 宛先選択のラジオボタンが３(新規入力)だった場合
-  def order_form_params
-    params.permit(:payment_method, :shipping_name, :shipping_postal_code, :shipping_address)
-  end
+	# 宛先選択のラジオボタンが３(新規入力)だった場合
+	def order_form_params
+		params.permit(:payment_method, :shipping_name, :shipping_postal_code, :shipping_address)
+	end
 
-  def order_params
-    params.permit(:customer_id, :shipping_name, :shipping_postal_code, :shipping_address, :shipping_fee, :total_payment, :payment_method)
-  end
+	def order_params
+		params.permit(:customer_id, :shipping_name, :shipping_postal_code, :shipping_address, :shipping_fee, :total_payment, :payment_method)
+	end
+
+	def shipping_params
+		params.permit(:name, :postal_code, :address)
+	end
 
 end
